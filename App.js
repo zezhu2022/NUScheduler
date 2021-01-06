94c11bb498910cbedc3a3dc1b1645e8be35c263a
Ze Zhu <zezhu2022@u.northwestern.edu>
2021-01-06 16:55:12 -0600

    modularization commit

--- App.js
+++ App.js
@@ -1,28 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+import CourseList from './components/CourseList';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  "title": "CS Courses for 2018-2019",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
 };
-
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.bannerStyle}>{schedule.title}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
   );
 }
+const Banner = props => (
+  <Text style={styles.bannerStyle}>{props.title}</Text>
+)
+  
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
-  bannerStyle: {
-    color: '#888',
-    fontSize: 32,
-  }
-});
+  courseList: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+  });
 
-export default App;
+  export default App;
---
+++ components/CourseList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Course from './Course';
+import { StyleSheet, ScrollView, View} from 'react-native';
+
+const CourseList = ({courses}) => (
+    <ScrollView>
+    <View style={styles.courseList}>
+      { courses.map(course => <Course key={course.id} course={course} />) }
+    </View>
+  </ScrollView>
+  );
+
+  const styles = StyleSheet.create({
+    courseList: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      },
+    });
+
+  export default CourseList;
